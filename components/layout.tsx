5f39679e8aa76d621b9525ab91dbae8be897ffcd
chaya2z <chaya2zdev@gmail.com>
2022-01-04 04:24:27 +0900

    [ Fix ] ogp image not display

--- components/layout.tsx
+++ components/layout.tsx
@@ -25,7 +25,7 @@ export default function Layout({
                 />
                 <meta
                     property="og:image"
-                    content="main_ogp.png"
+                    content="https://github.com/chaya2z.github.io/blob/gh-pages/main_ogp.png?raw=true"
                 />
                 <meta name="og:title" content={siteTitle} />
                 <meta name="twitter:card" content="summary_large_image" />
