b6213e5ffb0026771b729368dbce57ea3ff2d917
chaya2z <chaya2zdev@gmail.com>
2022-01-05 18:11:17 +0900

    [ Add ] all posts page

---
+++ pages/posts.tsx
@@ -0,0 +1,53 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+import Date from "../components/date";
+import { getSortedPostsData } from "../lib/posts";
+import { GetStaticProps } from "next";
+
+export const getStaticProps: GetStaticProps = async () => {
+    const allPostsData = getSortedPostsData();
+    return {
+        props: {
+            allPostsData,
+        },
+    };
+};
+
+export default function Home({ allPostsData }) {
+    return (
+        <Layout home>
+            <Head>
+                <title>{siteTitle} 投稿記事一覧</title>
+            </Head>
+            <section
+                className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
+            >
+                <h2 className={utilStyles.headingLg}>すべての記事一覧</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(
+                        ({ id, year, month, date, created_at, title }) => {
+                            return (
+                                <li
+                                    className={utilStyles.listItem}
+                                    key={created_at}
+                                >
+                                    <Link
+                                        href={`/posts/${year}/${month}/${date}/${id}`}
+                                    >
+                                        <a>{title}</a>
+                                    </Link>
+                                    <br />
+                                    <small className={utilStyles.lightText}>
+                                        <Date dateString={created_at} />
+                                    </small>
+                                </li>
+                            )
+                        }
+                    )}
+                </ul>
+            </section>
+        </Layout>
+    );
+}
