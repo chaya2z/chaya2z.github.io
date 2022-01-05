d5ee3f08d4c5c632afedcfe6bccc3fc7f776d5a1
chaya2z <chaya2z@users.noreply.github.com>
2022-01-05 09:12:48 +0000

    deploy: b6213e5ffb0026771b729368dbce57ea3ff2d917

---
+++ _next/static/V0vEHVV3qJ9Wu-aaxGAWq/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fposts","\u002Fposts\u002F[year]","\u002Fposts\u002F[year]\u002F[month]","\u002Fposts\u002F[year]\u002F[month]\u002F[date]","\u002Fposts\u002F[year]\u002F[month]\u002F[date]\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
