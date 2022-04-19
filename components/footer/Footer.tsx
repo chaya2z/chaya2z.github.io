d129a065b3b12c41257349cf444f9b29fffc9019
chaya2z <chaya2zdev@gmail.com>
2022-04-20 03:10:48 +0900

    [ Add ] footer component

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import footerStyles from "./footer.module.css";
+
+export default function Footer() {
+    return (
+        <div className={footerStyles.container}>
+            <small>&copy; 海底タランテラ chaya2z</small>
+        </div>
+    )
+}
