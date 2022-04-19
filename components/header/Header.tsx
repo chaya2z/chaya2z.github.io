974ef758909416ef253408e5243b7d3a7a75b23a
chaya2z <chaya2zdev@gmail.com>
2022-04-20 03:10:57 +0900

    [ Add ] header component

---
+++ components/header/Header.tsx
@@ -0,0 +1,9 @@
+import headerStyles from "./header.module.css";
+
+export default function Header() {
+    return (
+        <div className={headerStyles.container}>
+            <h1>海底タランテラ</h1>
+        </div>
+    )
+}
