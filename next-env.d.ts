d3a95d51dd4d83cf7abfc747ab54839b7522cafa
chaya2z <chaya2zdev@gmail.com>
2022-04-21 13:35:23 +0900

    [ Remove ] next-env config

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,8 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-
-declare module '*.css' {
-    const styles: { [className: string]: string };
-    export default styles;
-}
