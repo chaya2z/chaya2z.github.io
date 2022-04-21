e1944510c051f61d853d18de6b2d1e4d18de9664
chaya2z <chaya2zdev@gmail.com>
2022-04-21 14:17:34 +0900

    [ Add ] powered by GitHub Pages text

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -4,6 +4,7 @@ export default function Footer() {
     return (
         <div className={footerStyles.container}>
             <small>&copy; 海底タランテラ chaya2z</small>
+            <div className={footerStyles.text}>Powered by GitHub Pages</div>
         </div>
     )
 }
