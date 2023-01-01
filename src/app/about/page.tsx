440bf85c34b11519bff2217a98ad672bd2dda2ff
chaya2z <chaya2zdev@gmail.com>
2023-01-01 21:50:30 +0900

    [ Update ] About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,13 @@
+'use client';
+
+import { Heading, Text } from '@primer/react';
+
 const About = () => {
   return (
     <section>
-      <h2>About</h2>
-      <p>こんにちは。茶屋辻 ( chaya2z ) です。</p>
-      <p>このブログではネットワーク・サーバなどの IT 技術を中心にいろいろ投稿していく予定です。</p>
+      <Heading>About</Heading>
+      <Text as={'p'}>こんにちは。chaya2z です。</Text>
+      <Text as={'p'}>このブログでは IT 技術や日記を投稿しています。個人的な備忘録が中心です。</Text>
     </section>
   );
 };
