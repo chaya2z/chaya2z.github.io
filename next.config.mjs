31d2551a353a9862bbe44b5716263e99435ce3c0
chaya2z <chaya2zdev@gmail.com>
2023-04-30 23:22:49 +0900

    [ Update ] next config

--- next.config.mjs
+++ next.config.mjs
@@ -6,14 +6,16 @@ import addMdx from '@next/mdx';
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-        mdxRs: true,
-    },
+    images: { unoptimized: true },
     // Configure pageExtensions to include md and mdx
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     // Optionally, add any other Next.js config below
     reactStrictMode: true,
+    trailingSlash: true,
+    experimental: {
+        appDir: true,
+        mdxRs: true,
+    },
     output: 'export',
 };
 
